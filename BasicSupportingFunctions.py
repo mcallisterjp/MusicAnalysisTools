348a13f5786c1ddda35d704ca6f3478af72d1def
MarkGotham <mrhgotham@gmail.com>
2018-05-29 15:00:39 +0100

    Small additions

--- BasicSupportingFunctions.py
+++ BasicSupportingFunctions.py
@@ -1,14 +1,48 @@
+import unittest
+
 import os
 import pickle
+import csv
+
+import numpy as np
 import math
 import scipy.stats
 
+import matplotlib.pyplot as plt
+
+#------------------------------------------------------------------------------
+
 #Maths
+
+def monteCarlo(data, function, iterations=100, sampleSize=50, value=5, proportion=0.05):
+    '''
+    Conducts a basic Monte Carlo test for input data and function type.
+    Returns the count of successful trials,
+    the proportion overall,
+    and True/False for the meeting of conditions.
+    '''
+
+    dataPoints = len(data)
+    resultCount = 0
+
+    for i in range(iterations):
+        thisSample = random.sample(data, sampleSize)
+        result = function(thisSample)
+        if result < value:
+            resultCount +=1
+
+    proved = False
+    actualProportion = resultCount / iterations
+    if actualProportion < proportion:
+        proved = True
+
+    return resultCount, actualProportion, proved
 
 def getPValueBinomial(first, second, nullHyp=0.5):
     '''
     Returns the PValue for a distribution of two sets of T, F, or two-bin data.
     '''
+
     if first < second:
         first, second = second, first
     total = first + second
@@ -27,6 +61,26 @@ def getPercentile(l, amount=0.9):
     topFivePercent = round(lLen * topHalfAmount)
     return l[bottomFivePercent:topFivePercent + 1]
 
+def lcm(x, y):
+    '''
+    Returns the lowest common multple of two values
+    '''
+
+    # Choose the greater number:
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+#------------------------------------------------------------------------------
+
 #Files
 
 def storePickle(obj, path, filename):
@@ -40,3 +94,70 @@ def loadPickle(path, filename):
     with open(filename, 'rb') as filein:
         obj = pickle.load(filein)
     return obj
+
+def writeCSV(dataList,
+             csvFilePath='/Users/',
+             csvFileName='Test.csv',):
+            # objectsOfInterest
+    '''
+    Writes data to a CSV file.
+    '''
+
+    with open(csvFilePath+csvFileName, 'w') as csvfile:
+        csvOut = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for entry in dataList:
+            csvOut.writerow([x for x in entry])
+
+def getFiles(path='/Users/', extension=None):
+    '''
+    Retrieves files in the listed directory. So:
+    >>> fileList = getFiles(directory)
+    >>> for fileName in fileList:
+    >>>     [Any of the other functions]
+    '''
+
+    fileList = []
+    for file in os.listdir(path):
+        if extension:
+            if file.endswith(extension):
+                fileList.append(file)
+        else:
+            fileList.append(file)
+    return fileList
+
+#------------------------------------------------------------------------------
+
+#Patterns
+
+def match(longList, shortListOrLists, print=True):
+    for i in range(len(longlist) - len(shortlist)):
+        hitLists = []
+        currentPattern = longlist[i:i+len(shortlist)]
+        if currentPattern in shortListOrLists:
+            hitLists.append(currentPattern)
+            if print:
+                print(currentPattern)
+
+    tupleList = map(tuple, hitLists)
+    countList = Counter(tupleList)
+
+    return countList
+
+#------------------------------------------------------------------------------
+
+#Patterns
+
+class Test(unittest.TestCase):
+
+    def testMatch(self):
+
+        longList = [4, 6, 2, 4, 4, 4, 12, 4, 6, 2, 4,]
+        shortLists = [[4, 6], [2, 4]]
+
+        test = match(longList, shortLists)
+
+        self.assertIsInstance(test, dict)
+        self.assertEqual(test[shortLists[0]],2) # Check. shortLists[0] as key in test (dict), value 2
+
+#------------------------------------------------------------------------------
--- PlottingFunctions.py
+++ PlottingFunctions.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#------------------------------------------------------------------------------
+
 #Linear
 def linearFunc(x, a, b):
     return a*x+b
@@ -17,10 +19,13 @@ def polyFunc(x, a, b, c, d):
 def arcTanFunc(x, a, b, c, d):
     return a*np.arctan(b*x + c) + d
 
+#------------------------------------------------------------------------------
+
 def histogramOfIntervals(intervalList, xLabel=None, yLabel=None, title=None):
     '''
     Plotting shortcut for case of interval list.
     '''
+
     if xLabel is None:
         xLabel = 'Interval number'
     if yLabel is None:
@@ -45,3 +50,38 @@ def histogramOfIntervals(intervalList, xLabel=None, yLabel=None, title=None):
     plt.xticks(np.arange(min(intervalList), max(intervalList) + 1, 1),)
 
     plt.show()
+
+#------------------------------------------------------------------------------
+
+def dateRages(corps):
+    '''
+    Takes a list of lists of the type [name, date start, date end] and
+    returns a histograme for visualisation.
+    '''
+
+    size = len(corps) # No. of rows
+
+    corpora = sorted(corps, key = lambda x: int(x[1]))
+
+    plt.figure()#figsize=(5, 2))
+
+    x_values = []
+    y_values = []
+    y_axis = np.arange(0.5, size, 1)
+
+    # hlines and ylables
+    for i in range(size):
+        y_values.append(corpora[i][0])
+        l = plt.hlines(y=0.5 + i, xmin=corpora[i][1], xmax=corpora[i][2], linewidth=8, color='#d62728')
+
+    # plt.plot()
+    plt.yticks(y_axis, y_values)
+
+    plt.axis([1825, 2025, 0, size]) # TODO start date as variable: corpora[0][0]?
+    plt.title('Date Ranges', fontsize=20)
+    plt.xlabel('Date', fontsize=14)
+    # plt.ylabel('Corpora', fontsize=14)
+
+    plt.show()
+
+#------------------------------------------------------------------------------
